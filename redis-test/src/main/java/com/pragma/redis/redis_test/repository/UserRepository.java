20b874f38ce7923b9c64d41389bd875b73a1b4e8
juan pizo <josepizoc@gmail.com>
2024-10-25 00:52:47 -0500

    Primera versión del proyecto

---
+++ redis-test/src/main/java/com/pragma/redis/redis_test/repository/UserRepository.java
@@ -0,0 +1,47 @@
+package com.pragma.redis.redis_test.repository;
+
+import java.util.List;
+import java.util.Map;
+ 
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.pragma.redis.redis_test.model.User;
+ 
+ 
+@Repository
+public class UserRepository {
+    
+    public static final String USER_KEY = "USER";
+ 
+    private HashOperations hashOperations;
+ 
+    private RedisTemplate redisTemplate;
+ 
+    public UserRepository(RedisTemplate redisTemplate){
+        this.redisTemplate = redisTemplate;
+        this.hashOperations = this.redisTemplate.opsForHash();
+    }
+ 
+    public void save(User user) {
+        hashOperations.put(USER_KEY, user.getId(), user);
+    }
+ 
+    public List findAll(){
+        return hashOperations.values(USER_KEY);
+    }
+ 
+    public User findById(String id) {
+        return (User) hashOperations.get(USER_KEY, id);
+    }
+ 
+    public void update(User user) {
+        save(user);
+    }
+ 
+    public void delete(String id) {
+        hashOperations.delete(USER_KEY, id);
+    }
+ 
+}
